7f90094899d981154e43a5cac9e3c3a3fd312dbd
Magdiel Soto <magdielsoto18@gmail.com>
2022-12-22 19:04:30 -0300

    primer comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { PrimeNGModule } from './prime-ng/prime-ng.module'; este ya no se importa aqui sino que se importara en cada modulo que nesesite algo de primeNG
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
